efc49492d1efb47d0057fae0bfa7f133b3ffabf2
Uncle Data <tomas1991@gmail.com>
2023-12-22 15:23:10 +0200

    small tweaks

--- rust/day1/src/main.rs
+++ rust/day1/src/main.rs
@@ -10,7 +10,8 @@ fn main() {
         .lines()
     {
         sum += get_first_last_digits(line.to_owned());
-        sum2 += get_first_last_digits(adjust_line_get_digits(line.to_owned()));
+        let line = adjust_line_get_digits(line);
+        sum2 += get_first_last_digits(line.to_owned());
     }
     println!("Part1: {}", sum);
     println!("Part2: {}", sum2);
@@ -20,9 +21,8 @@ fn main() {
     println!("Elapsed time: {:?}", elapsed_time);
 }
 
-fn adjust_line_get_digits(inp: String) {
-    let inp = inp
-        .replace("one", "o1e")
+fn adjust_line_get_digits(inp: &str) -> String {
+    inp.replace("one", "o1e")
         .replace("two", "t2o")
         .replace("three", "t3e")
         .replace("four", "f4r")
@@ -31,21 +31,20 @@ fn adjust_line_get_digits(inp: String) {
         .replace("seven", "s7n")
         .replace("eight", "e8t")
         .replace("nine", "n9e")
-        .replace("zero", "z0o");
+        .replace("zero", "z0o")
 }
 
-fn get_first_last_digits(inp: String) -> i32 {
+fn get_first_last_digits(inp: String) -> u32 {
     let inp = inp.chars();
-    let mut first_dig = -1;
-    let mut last_dig = -1;
+    let mut first_dig = 10;
+    let mut last_dig = 10;
     for i in inp {
         if i.is_ascii_digit() {
-            if first_dig == -1 {
-                first_dig = i.to_digit(10).expect("not an int of base 10") as i32;
+            if first_dig == 10 {
+                first_dig = i.to_digit(10).expect("not an int of base 10") as u8;
             }
-            last_dig = i.to_digit(10).expect("not an int of base 10") as i32;
+            last_dig = i.to_digit(10).expect("not an int of base 10") as u8;
         }
     }
-    let res = (first_dig * 10) + last_dig;
-    return res;
+    (first_dig * 10 + last_dig) as u32
 }

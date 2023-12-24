b9cf6a3ddba2c5a4e514ff04275b64bd99c8f822
Uncle Data <tomas1991@gmail.com>
2023-12-24 11:14:34 +0200

    day1 reformat

--- python/day1.py
+++ python/day1.py
@@ -1,67 +1,42 @@
-from helpers import read_file_str, download_input, INPUTS_PATH, DEMO_PATH
+from helpers import read_file_str, INPUTS_PATH, DEMO_PATH
 
 DAY = 1
 IS_PROD = True
 
 
-def day1_p1(lines):
-    tmp = 0
-    sum_val = 0
-    for line in lines:
-        for char in line:
-            if char.isdigit():
-                tmp = int(char) * 10
-                break
-        for char in reversed(line):
-            if char.isdigit():
-                tmp = tmp + int(char)
-                break
-        sum_val += int(tmp)
-    print(sum_val)
+def get_first_last_digits(inp: str) -> int:
+    first_dig: int = 10
+    last_dig: int = 10
+    for ch in inp:
+        if ch.is_digit():
+            if first_dig == 10:
+                first_dig = int(ch)
+            last_dig = int(ch)
+    return first_dig * 10 + last_dig
 
 
-def get_nums(str_val):
-    nums = ""
-    vals = {
-        "one": 1,
-        "two": 2,
-        "three": 3,
-        "four": 4,
-        "five": 5,
-        "six": 6,
-        "seven": 7,
-        "eight": 8,
-        "nine": 9,
-    }
-    cur_idx = 0
-    while cur_idx < len(str_val):
-        for check in list(vals.keys()):
-            if str_val[cur_idx : cur_idx + len(check)].startswith(check):
-                nums = nums + str(vals[check])
-                # Not skipping last letter of check because it might be a start for another
-                cur_idx += len(check) - 2
-                break
-            elif str_val[cur_idx].isdigit():
-                nums = nums + str_val[cur_idx]
-                break
-        cur_idx += 1
-    return nums
-
-
-def day1_p2(lines):
-    tmp = 0
-    for line in lines:
-        nums = get_nums(line)
-        tmp += int(nums[0] + nums[-1])
-    print(tmp)
+def adjust_line_get_digits(inp: str) -> str:
+    return (
+        inp.replace("one", "o1e")
+        .replace("two", "t2o")
+        .replace("three", "t3e")
+        .replace("four", "f4r")
+        .replace("five", "f5e")
+        .replace("six", "s6x")
+        .replace("seven", "s7n")
+        .replace("eight", "e8t")
+        .replace("nine", "n9e")
+        .replace("zero", "z0o")
+    )
 
 
 if __name__ == "__main__":
     file_name = (
         DEMO_PATH.format(str(DAY)) if not IS_PROD else INPUTS_PATH.format(str(DAY))
     )
-    if IS_PROD:
-        download_input(str(DAY))
     lines = read_file_str(file_name)
-    day1_p1(lines)
-    day1_p2(lines)
+    sm1 = 0
+    sm2 = 0
+    for line in lines:
+        sm1 += get_first_last_digits(line)
+        sm2 += get_first_last_digits()
